dc810606d2c6ea299de4915b86ae940e490a1389
Abudhahir A <168201759+btwitsabu@users.noreply.github.com>
2024-05-20 23:15:10 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="" />
-        <p>SARTHAR</p>
+        <p>AARIZTRENDZ</p>
       </div>
       <img className="nav-dropdown" onClick={dropdown_toggle} src={nav_dropdown} alt="" />
       <ul ref={menuRef} className="nav-menu">
